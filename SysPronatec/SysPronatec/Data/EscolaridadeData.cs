183cd88f7b64dc1fbf608a108d416b44642db750
David Silva <davidhenriquesi@live.com>
2015-04-08 16:24:30 -0300

    Lista e adicionar pronto

--- SysPronatec/SysPronatec/Data/EscolaridadeData.cs
+++ SysPronatec/SysPronatec/Data/EscolaridadeData.cs
@@ -3,10 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SysPronatec.Model;
+using System.Data.Entity.Core.Objects;
+
 
 namespace SysPronatec.Data
 {
     class EscolaridadeData
     {
+
+		private SysPronatecEntities db = new SysPronatecEntities();
+
+		private  ObjectSet<Escolaridade> escolaridades;
+
+
+		public List<Escolaridade> todasEscolaridades(){
+		
+		
+			var lista = from es in db.Escolaridade
+			            select es;
+			return lista.ToList ();
+		
+		
+		}
+
+		public string adicionarEscolaridade(Escolaridade es){
+		
+				string erro = null;
+				try{
+
+					db.Escolaridade.Add(es);
+					db.SaveChanges();
+
+				}catch(Exception ex){
+
+				erro = ex.Message;
+
+
+				}
+			
+		
+		
+		}
+
+
+
+
     }
 }
