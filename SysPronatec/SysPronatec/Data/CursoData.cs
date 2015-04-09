505e5aeca996768017a62628d2d264602ffb9509
David Silva <davidhenriquesi@live.com>
2015-04-09 16:53:48 -0300

    Agora Sim Ate Segunda

--- SysPronatec/SysPronatec/Data/CursoData.cs
+++ SysPronatec/SysPronatec/Data/CursoData.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SysPronatec.Model;
 
 namespace SysPronatec.Data
 {
     class CursoData
     {
+        private SysPronatecEntities db = new SysPronatecEntities();
+
+
+        public List<Curso> todosCursos() {
+
+            var lista = from cu in db.Curso
+                        select cu;
+            return lista.ToList();
+        
+        
+        
+        }
+
+
+
+
+
+
     }
 }
