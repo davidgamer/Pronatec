31aa39c538aacd2fa9c7f267ebf65b53eea9d9a6
David Silva <davidhenriquesi@live.com>
2015-04-13 15:57:40 -0300

    Começãdo Views

--- SysPronatec/SysPronatec/Menu.cs
+++ SysPronatec/SysPronatec/Menu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SysPronatec
 {
-    public partial class Form1 : Form
+    public partial class SysPronatec : Form
     {
-        public Form1()
+        public SysPronatec()
         {
             InitializeComponent();
         }
--- SysPronatec/SysPronatec/Program.cs
+++ SysPronatec/SysPronatec/Program.cs
@@ -16,7 +16,8 @@ namespace SysPronatec
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SysPronatec());
+           
         }
     }
 }
